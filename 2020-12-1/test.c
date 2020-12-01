1a3c4fcc4bf0e8925a07d55282deb0f18957dd60
maggie <1070552760@qq.com>
2020-12-01 23:08:22 +0800

    2020-12-1/test.c练习

---
+++ 2020-12-1/test.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<string.h>
+#pragma warning (disable:4996)
+
+//#include <stdio.h>
+//#define __DEBUG__
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	for (i = 0; i<10; i++)
+//	{
+//		arr[i] = i;
+//        #ifdef __DEBUG__
+//		printf("%d\n", arr[i]);//为了观察数组是否赋值成功。
+//        #endif //__DEBUG__
+//	}
+//		return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int array[ARRAY_SIZE];
+//	int i = 0;
+//	for (i = 0; i< ARRAY_SIZE; i++)
+//	{
+//		array[i] = i;
+//	}
+//	for (i = 0; i< ARRAY_SIZE; i++)
+//	{
+//		printf("%d ", array[i]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
